3341b887fcc7eb95cf783e1ef151fba7f8ef1c62
Abner Persio <binhopersio@gmail.com>
2025-01-25 17:49:13 -0300

    refactor: lamdba utils transform

--- stacks/utils/lambda.ts
+++ stacks/utils/lambda.ts
@@ -1,28 +1,21 @@
 /// <reference path="../../.sst/platform/config.d.ts" />
 
-import { appConfig } from "../config";
-
 type Args = Omit<
   sst.aws.FunctionArgs,
   "architecture" | "nodejs" | "runtime" | "logging" | "transform"
 >;
 
-const getFunctionName = (handlerPath: string) => {
-  const handlerNameRegex = /.{1,}\/(.{1,}).(handler)/;
-  const name = handlerPath.replace(handlerNameRegex, "$1");
-  return `${appConfig.name}-${name}`;
-};
+// const getFunctionName = (handlerPath: string) => {
+//   const handlerNameRegex = /.{1,}\/(.{1,}).(handler)/;
+//   const name = handlerPath.replace(handlerNameRegex, "$1");
+//   return `${appConfig.name}-${name}`;
+// };
 
 export function lambda(args: Args): sst.aws.FunctionArgs {
   return {
     architecture: "arm64",
     memory: "128 MB",
     runtime: "nodejs22.x",
-    transform: {
-      function: {
-        name: getFunctionName(args.handler.toString()),
-      },
-    },
     nodejs: {
       esbuild: {
         bundle: true,
